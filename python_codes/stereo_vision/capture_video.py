da6d68d22131197e4d2bef3ba48d9d4a6858f976
msorour <mohamed.sorour.eg@gmail.com>
2018-11-24 04:06:57 +0000

    readme file updated

--- python_codes/stereo_vision/capture_video.py
+++ python_codes/stereo_vision/capture_video.py
@@ -5,16 +5,22 @@ import matplotlib.pyplot as plt
 
 # use : >> v4l2-ctl --list-formats-ext
 # to get info about camera attached
+
+# use : >> ls -ltrh /dev/video*
+# to get list of usb cameras attached
+
 # https://albertarmea.com/post/opencv-stereo-camera/
 
 left = cv2.VideoCapture(0)
 right = cv2.VideoCapture(1)
 #width = 1280
 #height = 720
-width = 800
-height = 600
+#width = 800
+#height = 600
 #width = 640
 #height = 480
+width = 320
+height = 240
 left.set(3, width)
 left.set(4, height)
 right.set(3, width)
