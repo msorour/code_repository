29dd2e7e93668e5eaa00983bcd819fed2f39c5e8
msorour <mohamed.sorour.eg@gmail.com>
2020-08-06 18:11:22 +0100

    repo update

--- ros_packages/devel/_setup_util.py
+++ ros_packages/devel/_setup_util.py
@@ -268,7 +268,7 @@ if __name__ == '__main__':
 
         if not args.local:
             # environment at generation time
-            CMAKE_PREFIX_PATH = '/home/business/code_repository/ros_packages/devel;/home/business/catkin_ws/devel;/opt/ros/melodic'.split(';')
+            CMAKE_PREFIX_PATH = '/home/experiments/ws_moveit/devel;/opt/ros/melodic'.split(';')
         else:
             # don't consider any other prefix path than this one
             CMAKE_PREFIX_PATH = []
